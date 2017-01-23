0d55aa31490071f4547aa72eefecc6dfa074d661
Bobby <Bobby@KISSINGER>
2017-01-23 12:55:42 -0500

    Add project files.

---
+++ MagicHateBall/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicHateBall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
